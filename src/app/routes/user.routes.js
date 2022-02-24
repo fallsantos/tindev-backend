52046cd7256ae7651268577fc1598c83b8bcd332
fallsantos <fallsantosdev@hotmail.com>
2022-02-23 22:01:37 -0300

    :construction: add user listing routes, likes and dislikes

--- src/app/routes/user.routes.js
+++ src/app/routes/user.routes.js
@@ -1,13 +1,14 @@
 const {Router} = require('express')
 
 const DevController = require('../controllers/DevController')
+const LikeControlelr = require('../controllers/LikeControlelr')
 
 const routes = Router()
 
 routes.post('/', DevController.store)
+routes.get('/', DevController.index)
 
-routes.get('/', (req, res) => {
-    return res.json({result: 'todos os usuários'})
-})
+routes.post('/:id/like', LikeControlelr.like)
+routes.post('/:id/dislike', LikeControlelr.dislike)
 
 module.exports = routes
