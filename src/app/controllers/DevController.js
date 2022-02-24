47c625ebc305e4cc0008db195f4f406b20595248
fallsantos <fallsantosdev@hotmail.com>
2022-02-23 22:02:43 -0300

    :construction: add function that lists new users

--- src/app/controllers/DevController.js
+++ src/app/controllers/DevController.js
@@ -1,6 +1,7 @@
 const axios = require('axios')
 
 const Dev = require('../models/Dev')
+const { use } = require('../routes/user.routes')
 
 class DevController {
     async store(req, res) {
@@ -15,7 +16,7 @@ class DevController {
             })  
         }
 
-        const response = await axios.get(`https://api.github.com/users/${username}`)
+        const response = await axios.get(`https://api.github.com/users/${user}`)
 
         const {name, bio, avatar_url: avatar } = response.data
 
@@ -32,7 +33,25 @@ class DevController {
         })
     }
 
+    async index(req, res) {
+        const { user } = req.headers
 
+        const loggedUser = await Dev.findById({ _id: user })
+
+        const users = await Dev.find({
+            $and: [
+                { _id: { $ne: user } },
+                { _id: { $nin: loggedUser.likes } },
+                { _id: { $nin: loggedUser.dislikes }}
+            ]
+        })
+
+        return res.status(400).json({
+            status: true,
+            message: '',
+            data: users
+        })
+    }
 }
 
 module.exports = new DevController()
